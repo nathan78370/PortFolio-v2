c8fc38a761e3905e8232aa3c87b5ed127accb9e5
LibertyGang <49068085+nathan78370@users.noreply.github.com>
2021-03-10 12:35:32 +0100

    modif functions

--- assets/includes/functions.php
+++ assets/includes/functions.php
@@ -1,7 +1,7 @@
 <?php
 $db_host="localhost"; //localhost server 
 $db_user="root";	//database username
-$db_password="root";	//database password   
+$db_password="root";	//database password  tentative
 $db_name="portfolio";	//database name
 
 try
